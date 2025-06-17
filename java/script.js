69f6ac9d191703068680ce6e2273837008a51cb5
Isaac Evans <mightysteve101@gmail.com>
2025-06-16 21:56:38 -0600

    Updated header

--- java/script.js
+++ java/script.js
@@ -1,48 +1,38 @@
 document.addEventListener("DOMContentLoaded", function(){
-	// Select DOM elements
-	const bodyElement = document.querySelector('body');
-	const navbarMenu = document.querySelector('#cs-navigation');
-	const hamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+// add classes for mobile navigation toggling
+    var CSbody = document.querySelector("body");
+    const CSnavbarMenu = document.querySelector("#cs-navigation");
+    const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
-	// Function to toggle the aria-expanded attribute
-	function toggleAriaExpanded(element) {
-	    const isExpanded = element.getAttribute('aria-expanded');
-	    element.setAttribute('aria-expanded', isExpanded === 'false' ? 'true' : 'false');
-	}
+    CShamburgerMenu.addEventListener('click', function() {
+        CShamburgerMenu.classList.toggle("cs-active");
+        CSnavbarMenu.classList.toggle("cs-active");
+        CSbody.classList.toggle("cs-open");
+        // run the function to check the aria-expanded value
+        ariaExpanded();
+    });
 
-	// Function to toggle the menu open or closed
-	function toggleMenu() {
-	    hamburgerMenu.classList.toggle('cs-active');
-	    navbarMenu.classList.toggle('cs-active');
-	    bodyElement.classList.toggle('cs-open');
-	    toggleAriaExpanded(hamburgerMenu);
-	}
+    // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
+    function ariaExpanded() {
+        const csUL = document.querySelector('#cs-expanded');
+        const csExpanded = csUL.getAttribute('aria-expanded');
 
-	// Add click event listener to the hamburger menu
-	hamburgerMenu.addEventListener('click', toggleMenu);
+        if (csExpanded === 'false') {
+            csUL.setAttribute('aria-expanded', 'true');
+        } else {
+            csUL.setAttribute('aria-expanded', 'false');
+        }
+    }
 
-	// Add click event listener to the navbar menu to handle clicks on the pseudo-element
-	navbarMenu.addEventListener('click', function (event) {
-	    if (event.target === navbarMenu && navbarMenu.classList.contains('cs-active')) {
-	        toggleMenu();
-	    }
-	});
-
-	// If you press Escape and the hamburger menu is open, close it
-	document.addEventListener('keydown', (event) => {
-	    if (event.key === 'Escape' && hamburgerMenu.classList.contains('cs-active')) {
-	        toggleMenu();
-	    }
-	});
-
-	/***************FAQ*********************/
-	const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
-        for (const item of faqItems) {
+    // mobile nav toggle code
+    const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+        for (const item of dropDowns) {
             const onClick = () => {
-            item.classList.toggle('active')
+            item.classList.toggle('cs-active')
         }
         item.addEventListener('click', onClick)
         }
+                                
                                 
                                 
 })
