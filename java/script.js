994edbf2678661ba9bf66f8d7d00cf503cd5eaab
Isaac Evans <mightysteve101@gmail.com>
2025-06-16 22:39:53 -0600

    Updated Nav and font
    Still looking for a nav bar that fits, and changed the look of the font.

--- java/script.js
+++ java/script.js
@@ -24,6 +24,20 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+        // This script adds a class to the body after scrolling 100px
+    // and we used these body.scroll styles to create some on scroll 
+    // animations with the navbar
+    
+    document.addEventListener('scroll', (e) => { 
+        const scroll = document.documentElement.scrollTop;
+        if(scroll >= 100){
+    document.querySelector('body').classList.add('scroll')
+        } else {
+        document.querySelector('body').classList.remove('scroll')
+        }
+    });
+
+
     // mobile nav toggle code
     const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
         for (const item of dropDowns) {
@@ -32,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         item.addEventListener('click', onClick)
         }
+                                
                                 
                                 
                                 
